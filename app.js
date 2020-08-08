f13fd6970465e5085d6cfd8985c07673d0a95b22
sumedhchilak <43299189+sumedhchilak@users.noreply.github.com>
2020-08-08 11:40:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ var user = {
 var resultsPage = [];
 var videos = [];
 var workoutId = ['workoutResults1', 'workoutResults2', 'workoutResults3'];
-
 var upperBeg = ["Kneeling Pushups", "Shoulder Taps", "Crunches"];
 var upperInt = ["Regular Pushups", "Supermans", "Leg Raises"];
 var upperAdv = ["Diamond Pushups", "Dips", "V-Ups"];
 var lowerBeg = ["Glute Briges", "Lunges", "Crunches"];
 var lowerInt = ["Donkey Kicks", "box jumps", "leg Raises"];
 var lowerAdv = ["Deadlifts", "Pistols", "V-Ups"];
-
 var upperBegVid = ["https://www.youtube.com/embed/wc-W05Gi9hU", "https://www.youtube.com/embed/gWHQpMUd7vw", "https://www.youtube.com/embed/HiRsmHH7psA"];
 var upperIntVid = ["https://www.youtube.com/embed/v9LABVJzv8A", "https://www.youtube.com/embed/67rgxYNKbZY", "https://www.youtube.com/embed/_OQaO65Vdzs"];
 var upperAdv = ["https://www.youtube.com/embed/SwoNNo4W1OU", "https://www.youtube.com/embed/vhXqTx7JYSs", "https://www.youtube.com/embed/-JIwvMSk4vo"];
 var lowerBegVid = ["https://www.youtube.com/embed/N48d7sm8dbU", "https://www.youtube.com/embed/UpyDdQjBTa0", "https://www.youtube.com/embed/HiRsmHH7psA"];
 var lowerIntVid = ["https://www.youtube.com/embed/SJ1Xuz9D-ZQ", "https://www.youtube.com/embed/52r_Ul5k03g", "https://www.youtube.com/embed/_OQaO65Vdzs"];
 var lowerAdvVid = ["https://www.youtube.com/embed/HtHxnWmMgzM", "https://www.youtube.com/embed/qDcniqddTeE", "https://www.youtube.com/embed/-JIwvMSk4vo"];
-
+//Recieves User Data from the form
 function formData(){
   event.preventDefault();
   var name = document.getElementsByName('name')[0].value;
@@ -36,10 +34,12 @@ function formData(){
       user.type = workoutType[i].value;
     }
   }
+  // Stores the user data as a JSON string to be stored in local storage
   localStorage.setItem('userData', JSON.stringify(user));
+  // other method is called to generate the workout
   genWorkout();
 }
-
+// generates workout based on user data and stores the appropriate arrays as a JSON string in local storage
 function genWorkout(){
     if(user.type === 'Upper' && user.level === 'Beginner'){
         localStorage.setItem('workoutData', JSON.stringify(upperBeg));
@@ -65,48 +65,60 @@ function genWorkout(){
         localStorage.setItem('workoutData', JSON.stringify(lowerAdv));
         localStorage.setItem('workoutVideo', JSON.stringify(lowerAdvVid));  
     }
+    // Allows the page to redirect to result
     window.location = 'result.html';
 }
-
+// Returns the element that has this ID attribute
 document.getElementById('clickMe');
+// Attaches event handler to the document
 document.addEventListener('submit', formData);
-
 function createWorkoutPage(){
+  // Information stored again after parsing the JSON string that was stored in local storage
   resultsPage = JSON.parse(localStorage.getItem('workoutData'));
   videos = JSON.parse(localStorage.getItem('workoutVideo'));
   user = JSON.parse(localStorage.getItem('userData'));
+  // Different aspects of the workout page
   doName();
   doResults(resultsPage);
   doVideo(resultsPage,videos);
 }
-
 function doResults(resultsPage){
   for(var i = 0; i < resultsPage.length; i++){
+    // Returns the element with the ID attribute resultsTitle
     var outlineBox = document.getElementById('resultsTitle');
+    // HTML element created
     var outline = document.createElement('div');
     outline.setAttribute('id', 'exerciseList');
+    // Sets the HYML content 
     outline.innerHTML = resultsPage[i] + ' for 8-12 reps';
+    // Appends node as the last child of a node
     outlineBox.appendChild(outline);
   }
 }
-
 function doVideo(resultsPage, videos){
   for(var i = 0; i < resultsPage.length; i++){
+    // Returns the element that has the ID attribute to this specific value
     var workoutContainer = document.getElementById('workoutResults');
+    // Creates the HTML element
     var vidContainer = document.createElement('div');
+    // Adds the specified attribute to the specified elements, giving it the value 
     vidContainer.setAttribute('class', 'workoutFrame');
     vidContainer.setAttribute('id', workoutId[i]);
     var video = document.createElement('iframe');
+    // Adds the specified attribute to the specified elements, giving it the value   
     video.setAttribute('class', 'iframeSizing');
+    // Assigns appropriate embeded youtube link based on index in videos array
     video.src = videos[i];
+    // Appends node as the last child of a node
     workoutContainer.appendChild(vidContainer);
     vidContainer.appendChild(video);
   }
 }
-
 function doName(){
   var getName = document.getElementById('showName');
   var addName = document.createElement('p');
+  // Sets the HYML content
   addName.innerHTML = "YO " + user.name + ", it's time to start grinding!";
+  // Appends node as the last child of a node
   getName.appendChild(addName);
 }
